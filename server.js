f068a407ea5e7a1697ff9587e78db4035350c722
DenisUAL <denisandreev64@gmail.com>
2017-09-20 23:19:23 -0400

    tables for client and associate

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const bodyParser = require('body-parser');
 const PORT = 3000;
 const json2xls = require('json2xls');
 const fs = require('fs');
+const Sequelize = require('sequelize');
 var models = require('./models');
+var associateToComplete = models.associateToComplete;
+var clientToComplete = models.clientToComplete;
 
 app.use(volleyball);
 app.use(express.static('public'));
@@ -16,10 +19,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 app.get('/api', (req, res) => {
-    models.repairInvoice.findAll().then((result)=> res.json(result));
+    clientToComplete.findAll().then((result)=> res.json(result));
 })
-app.post('/api', (req, res) => {
-    models.repairInvoice.create(req.body);
+app.post('/api/client_form', (req, res) => {
+    clientToComplete.create(req.body);
 })
 app.post("/formSubmit", (req, res) => {
     console.log(req.body)
@@ -27,7 +30,7 @@ app.post("/formSubmit", (req, res) => {
     fs.writeFileSync('data.xlsx', xls, 'binary');
 })
 
-models.repairInvoice.sync({logging: false, force: true})
+models.db.sync({logging: false, force: true})
 .then(function () {
     app.listen(PORT, () => {
         console.log('listening on port', PORT)
